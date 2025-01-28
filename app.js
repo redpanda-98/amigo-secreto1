6151004f9cdfa0a928faf4ec79a7a3be312f0ead
Mosel <mosellarios@gmail.com>
2025-01-28 08:54:44 -0600

    Implementa una función para agregar amigos

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigo = document.getElementById('amigo');
+const agregarBtn = document.querySelector('.button-add');
+let nombres = [];
+
+function validarNombreAmigo(amigo){
+    let nombre = amigo.value;
+    if(nombre.length > 0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+function alertaNombreInvalido(){
+    alert('Por favor inserte un nombre');
+}
+
+function agregarAmigo(){
+    let nombre = amigo.value;
+    if(validarNombreAmigo(amigo) == true){
+        nombres.push(nombre);
+        limpiarInput();
+    }else{
+        alertaNombreInvalido();
+    }
+    console.log(nombres);
+}
+
+function limpiarInput(){
+    return amigo.value = '';
+}
+
